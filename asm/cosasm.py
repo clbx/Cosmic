9aeb86c74f94f2583bd7dc7a68ce803e4b60d1ba
Clay Buxton <clay@clbx.io>
2020-01-26 20:27:47 -0500

    assembler now will properly resolve variable names

--- asm/cosasm.py
+++ asm/cosasm.py
@@ -17,13 +17,15 @@ output = bytearray()
 bitnessPattern = re.compile("[A-Z]{3}[X].*$")
 #Addressing Modes
 impliedPattern = re.compile("[A-Z]{3,4}$")
-immmediatePattern = re.compile("[A-Z]{3,4} [#][0-9,A-F]{1,4}$")
-absolutePattern = re.compile("[A-Z]{3,4} [0-9,A-F]{1,4}$")
-indirectPattern = re.compile("[A-Z]{3,4} [@][0-9,A-F]{1,4}$")
+immmediatePattern = re.compile("[A-Z]{3,4} [#][0-9,A-Z,a-z]{1,4}$")
+absolutePattern = re.compile("[A-Z]{3,4} [0-9,A-Z,a-z]{1,4}$")
+indirectPattern = re.compile("[A-Z]{3,4} [@][0-9,A-Z,a-z]{1,4}$")
 registerPattern = re.compile("[A-Z]{3,4} [R][0-7]$")
 constantPattern = re.compile("(word|byte) [A-Z,a-z]{1}[A-Z,a-z,0-9]{0,128} [=] (0x)?[0-9,A-F,a-f]{1,4}")
 variablePattern = re.compile("var (word|byte) [A-Z,a-z]{1}[A-Z,a-z,0-9]{0,128} [=] (0x)?[0-9,A-F,a-f]{1,4}")
 hasVarConstPattern = re.compile("[A-Z]{3,4} (#|@|R)?[0-9,A-Z,a-z]{1,}$")
+hexNumberPattern = re.compile("[A-F,0-9]{1,4}")
+opcodePattern = re.compile("[A-Z,a-z]{3,4} [#,@,R]?[0-9,A-Z,a-z]{1,}([ ][#,@,R]?[0-9,A-Z,a-z]{1,})?$")
 
 labelPattern = re.compile("[A-Z,a-z,0-9]{1,}:$")
 
@@ -64,16 +66,27 @@ InstructionSet = {
     'JCS':[0x7C,0x7D,0x7E,0x7F],
     'JNC':[0x80,0x81,0x82,0x83],
     'JOS':[0x84,0x85,0x86,0x87],
-    'JNS':[0x88,0x89,0x8A,0x8B]
+    'JNO':[0x88,0x89,0x8A,0x8B],
+    'JNS':[0x8C,0x8D,0x8E,0x8F],
+    'JNN':[0x90,0x91,0x92,0x93],
+    'JLS':[0x94,0x95,0x96,0x97],
+    'JNL':[0x98,0x99,0x9A,0x9B]
 }
 
+#All of the instructions that use 16 bit operands
 x16Instructions = {
     'PUSH','CALL','ADDX','SUBX','MULX',
     'DIVX','SHLX','JMP','JZS','JNZ','JCS',
     'JNC','JOS','JNS','MOVXA','MOVXI','MOVXR'
 }
 
+#All of the jump instructions
+Jumps  = {
+    'JMP','JZS','JCS','JNC','JOS','JNO','JNS',
+    'JNN','JLS','JNL'
+}
 
+#All of the move instructions
 MovSet = {
     'MOVA':[0x30,0x31,0x32,0x33], #Moving to an Absolute Location
     'MOVI':[0x34,0x35,0x36,0x37], #Moving to an Indirect Location
@@ -85,20 +98,26 @@ MovSet = {
 
     
 #Returns the addressing mode
-def getAddrMode(instruction):
-    if(immmediatePattern.match(instruction) or impliedPattern.match(instruction)):
+def getAddrMode(tokens):
+    instruction = " ".join(tokens)
+    if(impliedPattern.match(instruction)):
         return 0
-    elif(absolutePattern.match(instruction)):
+    if(immmediatePattern.match(instruction)):
         return 1
-    elif(indirectPattern.match(instruction)):
+    elif(absolutePattern.match(instruction)):
         return 2
-    elif(registerPattern.match(instruction)):
+    elif(indirectPattern.match(instruction)):
         return 3
+    elif(registerPattern.match(instruction)):
+        return 4
     else:
         return -1
 
 
-
+'''
+Goes through the assembly code and picks out any variable declarations, then adds
+them to the front of the program to be used later
+'''
 def getVariables(tokens):
     if(tokens[1] == "byte" or tokens[1] == "word"):
         if(tokens[2] in variableTable):
@@ -116,19 +135,79 @@ def getVariables(tokens):
             error("Type {} is not a valid type (byte|word)".format(tokens[1]))     
 
 
+
+'''
+When a label declaration is found, it sets that labvel equal to the current position
+'''
+def handleLabel(tokens):
+    label = tokens[0][:-1]
+    if(label in labelTable):
+        error("Label {} alread in label table".format(label))
+    else:
+        labelTable[label] = len(output)
+
+'''
+Checks if a variable or label is present in a opcode call and replaces it with the correct value
+Don't touch it otherwise
+'''
+def checkVar(tokens):
+    #Check if it's a MOV instruciton, if it is the variables could be in different places and an extra place
+    if('MOV' in tokens[0]):
+        print("not implementing this yet")
+    #For all the other opcodes
+    else:
+        #We want to break down the operand, so we want to separate the #,R or @ from the rest of the operand
+        #We'll re add it at the end, unless its a label.
+        addrmode = getAddrMode(tokens)
+        operator = ""
+        operand = ""
+
+        if(addrmode == 0 or addrmode == 2):
+            operand = tokens[1]
+        else:
+            operator = tokens[1][0]
+            operand = tokens[1][1:]
+        print(addrmode)
+        print(operand)
+        print(operator)
+        #If the operand is a hex number, we can hop off the bus now
+        if(hexNumberPattern.match(operand)):
+            return tokens
+
+        #Check if it's a jump to check for lables, Jumps should be the only opcode using labels
+        if(tokens[0] in Jumps):
+            if(operand in labelTable):
+                tokens[1] = labelTable[operand]
+                return tokens
+            else:
+                error("Label {} not in label table".format(operand))
+            return
+
+        #Now for everyone else
+        elif(operand in variableTable):
+            if(operand in variableTable):
+                print(variableTable)
+                tokens[1] = str(operator) + str(output[variableTable[operand]])
+                return tokens
+            else:
+                error("Variable {} not in variable table".format(operand))
+
+        else:
+            return tokens
+
+
+
+
+            
+
+
+
+
 def handleOpcode(tokens):
     #Check if it has a variable in it
-    if(checkVar(tokens)):
-        if("MOV" in tokens[0]):
-            tokens = resolveVarMov(tokens)
-        else:
-            tokens = resolveVar(tokens)
-    #If no vars or vars resolved
-    if("MOV" in tokens[0]):
-        tokens = resolveVarMov(tokens)
-    else:
-        tokens = resolveVar(tokens)
-    
+    tokens = checkVar(tokens)
+    print("TOKENS" + str(tokens))
+    #handle the rest here
 
         
 
@@ -140,10 +219,14 @@ def assemble(tokens):
     if(tokens[0][:-1] == ":"):
         handleLabel(tokens)
     #If its an opcode
-    elif(xyz):
+    elif(opcodePattern.match(" ".join(tokens))):
         handleOpcode(tokens)
+    #If its a comment
     elif(tokens[0][0] == ";"):
-        continue
+        pass
+    #If its a variable assignement skip it
+    elif(tokens[1] == "byte" or tokens[1] == "word"):
+        pass
     else:
         error("Unknown thing")
     
