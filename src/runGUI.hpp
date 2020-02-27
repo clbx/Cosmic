063d33465b2d573ab9ede4441bbce939bde0a6f5
Clay Buxton <clay@clbx.io>
2020-02-27 00:28:44 -0500

    Added OpenGL2 support for Raspberry Pi (#44)

--- src/runGUI.hpp
+++ src/runGUI.hpp
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
-#include <GL/gl3w.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 
 #include "imgui.h"
 #include "imgui_impl_sdl.h"
-#include "imgui_impl_opengl3.h"
 #include "imgui_memory_editor.h"
 #include "imgui_logger.h"
 #include "imguifilesystem.h"
+
+#ifdef __arm__ //If on the Raspberry Pi 
+    #include "imgui_impl_opengl2.h"
+    #include <SDL_opengl.h>
+#else
+    #include "imgui_impl_opengl3.h"
+    #include <GL/gl3w.h>
+#endif
 
 #include "cosproc.hpp"
 
